e5f13304e83c33f6541a6eb1f43e93a820f94259
yangangzhang <913800916@qq.com>
2019-08-12 17:07:15 +0800

    Update sqlMap.js

--- service/db/sqlMap.js
+++ service/db/sqlMap.js
@@ -1,6 +1,6 @@
 var sqlMap = {
     user: {
-        add: 'insert into user (username, account, password, repeatPass, email, phone, card, birth, sex) values (?,?,?,?,?,?,?,?,?)',
+        add: 'insert into user (username, account, password, email, phone, card, birth, sex) values (?,?,?,?,?,?,?,?)',
         select_name: 'select * from user', 
         update_user: 'update user set'
     }
